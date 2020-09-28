1b26ff0ee81914ec6086582a947cc3f65d764ce7
Erik <erik851c@stud.kea.dk>
2020-09-28 14:20:16 +0200

    First commit

---
+++ src/test/java/group/six/adventurexp/AdventurexpApplicationTests.java
@@ -0,0 +1,13 @@
+package group.six.adventurexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventurexpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
