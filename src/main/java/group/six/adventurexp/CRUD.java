a55be7654ccfd924b947023efb1fca9106dcffb1
martinp98 <martin.p.munk@gmail.com>
2020-09-29 11:46:10 +0200

    While

--- src/main/java/group/six/adventurexp/CRUD.java
+++ src/main/java/group/six/adventurexp/CRUD.java
@@ -31,5 +31,10 @@ public class CRUD {
 
     }
 
+    public void read()
+    {
+
+    }
+
 
 }
--- src/main/java/group/six/adventurexp/UI.java
+++ src/main/java/group/six/adventurexp/UI.java
@@ -16,24 +16,37 @@ public class UI {
          "$$  |  $$ |  $$ |  $$ |  \\$$$  /  $$   ____|  $$ |  $$ |  $$ |$$ \\$$ |  $$ | $$  |      $$   ___|   $$ |        $$  __$$   | $$ |  $$  |  $$ |  $$ | \n" +
          "$$  |  $$ | \\$$$$$$$ |   \\$  /  \\$$$$$$$\\  $$ |  $$ | \\$$$$ | \\$$$$$$  | $$  |    \\$$$$$$$\\   $$$$$$$$\\ \\$$$$$$$   | $$ |  $$  | \\$$$$$$$ | \n" +
          "\\__|  \\__| \\_______|    \\_/    \\_______| \\__| \\__| \\____/   \\______/  \\__|     \\_______|  \\________| \\_______|   \\__|  \\__|  \\_______|");
+        System.out.println();
     }
 
     public void EmployeeUI() {
         Logo();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("You have the following options:");
-        System.out.println("1. Show Activities | 2. Create Activity | 3.Edit Activities | 4.Delete Activities");
+        while(true) {
+            System.out.println("You have the following options:");
+            System.out.println("1. Show Activities | 2. Create Activity | 3.Edit Activities | 4.Delete Activities | 0.Exit Application");
 
-        String scannerInput = sc.next();
+            String scannerInput = sc.next();
 
-        switch (scannerInput){
-            case "1":
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
+            switch (scannerInput) {
+                case "1":
+                    System.out.println("Vis");
+                    break;
+                case "2":
+                    System.out.println("create");
+                    break;
+                case "3":
+                    System.out.println("edit");
+                    break;
+                case "4":
+                    System.out.println("delete");
+                    break;
+                case "0":
+                    System.exit(0);
+                    break;
+            }
+
         }
     }
 }
