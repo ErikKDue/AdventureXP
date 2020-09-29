39b857659ddf10af00f89439ef080ccf2eb3902a
martinp98 <martin.p.munk@gmail.com>
2020-09-29 12:01:40 +0200

    read i crud

--- src/main/java/group/six/adventurexp/BrugerUI.java
+++ src/main/java/group/six/adventurexp/BrugerUI.java
@@ -12,12 +12,6 @@ public class BrugerUI {
         // write your code here
         CRUD crud = new CRUD();
 
-        ArrayList<Activity> aktiviteter = new  ArrayList<>(Arrays.asList(
-                new Activity("GoKart",500, "Kør rundt på vores fede GoKart bane, i vores super hurtige og sjove GoKarts!", "Du skal være 16 år gammel, og mindst 1,50m høj"),
-                new Activity("Paint Ball", 750, "Skyd dine venner i fjæset med maling", "Du skal være 18 år gammel, der er ingen højde begrænsning"),
-                new Activity("Mini Golf", 900, "Bliv frustreret over dine dårlige evner til at spille mini golf, og lad dine aggresioner gå ud over din familie", "Der er ingen krav")
-        ));
-
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Velkommen til Adventure XP! (slogan her)");
@@ -33,9 +27,7 @@ public class BrugerUI {
                 scan.nextLine();
             } else if (scan.hasNext("0")) {
 
-                for (int i = 0; i < aktiviteter.size(); i++) {
-                    aktiviteter.get(i).display();
-                }
+                crud.read();
 
 
                 scan.nextLine();
--- src/main/java/group/six/adventurexp/CRUD.java
+++ src/main/java/group/six/adventurexp/CRUD.java
@@ -1,8 +1,16 @@
 package group.six.adventurexp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CRUD {
+
+    ArrayList<Activity> aktiviteter = new  ArrayList<>(Arrays.asList(
+            new Activity("GoKart",500, "Kør rundt på vores fede GoKart bane, i vores super hurtige og sjove GoKarts!", "Du skal være 16 år gammel, og mindst 1,50m høj"),
+            new Activity("Paint Ball", 750, "Skyd dine venner i fjæset med maling", "Du skal være 18 år gammel, der er ingen højde begrænsning"),
+            new Activity("Mini Golf", 900, "Bliv frustreret over dine dårlige evner til at spille mini golf, og lad dine aggresioner gå ud over din familie", "Der er ingen krav")
+    ));
 
     public Activity Create()
     {
@@ -33,8 +41,23 @@ public class CRUD {
 
     public void read()
     {
-
+        for(int i = 0; i < aktiviteter.size(); i++)
+        {
+            //bruger sout til at printe information om aktiviteten i konsollen
+            System.out.println("");
+            System.out.println("Aktivitet: " + aktiviteter.get(i).getName());
+            //print titlen og prisen
+            System.out.println("Pris pr person " + aktiviteter.get(i).getPrice() +"dkk");
+            System.out.println(aktiviteter.get(i).getDescription());
+            if (aktiviteter.get(i).getRestriction() != null && !aktiviteter.get(i).getRestriction().equals("")) {
+                System.out.println("BEMÆRK \n" + aktiviteter.get(i).getRestriction());
+            }
+            System.out.println("");
+            //print beskrivelsen
+        }
     }
+
+
 
 
 }
